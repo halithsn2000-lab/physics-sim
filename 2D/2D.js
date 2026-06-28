c01de1ae33952ccf5e4d69db9da59adde7863e3e
halit <halithsn@gmail.com>
2026-06-29 12:02:38 +1300

    working on 2D projectile motion

---
+++ 2D/2D.js
@@ -0,0 +1,10 @@
+angle = 0;
+speed = 0;
+acceleration = 0;
+function clicked(){
+    speed = document.getElementById("iS").value;
+    angle = document.getElementById("angle").value;
+    acceleration = document.getElementById("A").value;  
+    console.log(speed , angle , acceleration);
+    
+}
