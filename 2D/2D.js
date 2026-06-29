3a4a941892dae39e8519be26418478c685960d7d
halit <halithsn@gmail.com>
2026-06-29 13:28:24 +1300

    made the basic 2d projectile motion logic

--- 2D/2D.js
+++ 2D/2D.js
@@ -1,10 +1,42 @@
-angle = 0;
-speed = 0;
-acceleration = 0;
+let angle = 0;
+let speed = 0;
+let acceleration = 0;
 function clicked(){
-    speed = document.getElementById("iS").value;
-    angle = document.getElementById("angle").value;
-    acceleration = document.getElementById("A").value;  
+
+    speed =Number(document.getElementById("iS").value);
+    angle = Number(document.getElementById("angle").value);
+    acceleration = Number(document.getElementById("A").value);  
     console.log(speed , angle , acceleration);
+    const angleRad = angle * Math.PI / 180;
+    let Vx = speed * Math.cos(angleRad);
+    let Vy = speed * Math.sin(angleRad);
+    let X = 0;
+    let Y = 1;
+    let dt = 0.16
+    const timer = setInterval(() => {
+
+        Vx = Vx + acceleration * dt;
+        Vy = Vy + -9.81 * dt;
+        X = X + Vx * dt;
+        Y = Y + Vy * dt;
+
+        document.getElementById("xx").innerHTML = Math.round(X);
+        document.getElementById("yy").innerHTML = Math.round(Y);
+
+        if (Math.round(Y) === 0) {
+            clearInterval(timer);
+        }
+
+
+
+
+
+
+},16);
+
+
+   
     
+
+
 }
